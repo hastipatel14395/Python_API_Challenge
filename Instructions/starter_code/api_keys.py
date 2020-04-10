313dff1fd14a346974e41dcaced524d990ac7ea6
Hasti Patel <60442442+hastipatel14395@users.noreply.github.com>
2020-04-10 15:29:16 -0400

    Initial Commit

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "1ef5b019201ca19d21fdd5bc26119301"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyADXZIn2ZZokKW92Kh9fy-LmwOuU6lyuQw"
